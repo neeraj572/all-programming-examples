e2b077216d0854bb4ce2e216dda6d513715419dc
Neeraj <99793365+neeraj572@users.noreply.github.com>
2022-02-20 23:17:14 +0530

    Update A.java

--- src/tokenizer/A.java
+++ src/tokenizer/A.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 public class A {
 	public static void main(String[] args) {
-		StringTokenizer str =new StringTokenizer("I Love You Anjali"," ");  //after the string we are writing double quotes based
+		StringTokenizer str =new StringTokenizer("my name is Neeraj Kumar"," ");  //after the string we are writing double quotes based
 		                                                         //on which string is being splitted that is called delimiter
 		int num=0;
 		while(str.hasMoreTokens()) {
